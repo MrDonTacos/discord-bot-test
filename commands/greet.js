e23cf1181ac99dd858ad07801fd3727d4fafe99d
Saul Cañedo <45052379+MrDonTacos@users.noreply.github.com>
2020-11-05 10:05:56 -0800

    Update greet.js
    Añadiendo comentarios que describen la funcionalidad de cada línea del archivo .js

--- commands/greet.js
+++ commands/greet.js
@@ -1,7 +1,7 @@
 module.exports = {
-    name: "greet",
-    description: "Greet someone",
+    name: "greet", //Se define el nombre del comando, este nombre será de ayuda para el comando help
+    description: "Greet someone", //Se describe la funcionalidad del comando, esta descripción describirá el comando en el comando help
     execute(message, args) {
-        message.channel.send("I'm greeting " + message.author.username)
+        message.channel.send("I'm greeting " + message.author.username) //Dentro del execute se define la lógica de lo que se quiere realizar con el comando
     }
 }
