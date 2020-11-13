2090f6560636b964eb057da523840eda1d2a42f7
Saúl Cañedo Castillo <saul.canedocast@hotmail.com>
2020-11-12 22:50:56 -0800

    Añadiendo get en get-list

--- commands/get-list.js
+++ commands/get-list.js
@@ -1,3 +1,7 @@
+const axios = require('axios');
+const { prefix } = require('../config.json');
+const List = require('../controller/listController.js');
+
 module.exports = {
     name: 'get-list',
     description: 'Obtiene la información de todas las listas o una lista del servidor', 
@@ -5,7 +9,14 @@ module.exports = {
 		if (args.length > 0) {
             return message.channel.send(`El comando no cumple con los parametros necesario, 
             para más información del comando consultar ${prefix}help`)
-		}
+        }
+        else {
+            const result = List.get().then(response => {
+                return response;
+            }).catch(error => {
+                return message.channel.send(`Surgió un error al obtener las listas`)
+            });
+        }
         
 	},
 };
